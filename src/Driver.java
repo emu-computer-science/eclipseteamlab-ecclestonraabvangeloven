e110041e2de5880aed7a94357de0ed6ca973ce92
jtvangeloven <26tamia@gmail.com>
2024-05-21 12:08:10 -0400

    The glad method commit

--- src/Driver.java
+++ src/Driver.java
@@ -2,5 +2,8 @@
 public class Driver {
 	public static void main(String [] args) {
 		System.out.println("Our team is getting started.");
+		
+		//Call the glad method from the Glad class
+		Glad.glad();
 	}
 }
